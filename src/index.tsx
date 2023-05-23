d85049fbce15a9fc7c990fc3b40e8b9d06c739ff
kimyou1102 <yoosk5485@naver.com>
2023-05-23 19:53:07 +0900

    chore: react-router-dom 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import GlobalStyle from './styles/globalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <RecoilRoot>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </RecoilRoot>,
 );
