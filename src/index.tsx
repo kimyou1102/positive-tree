de4515e2d031cf27367a45af69373ebc723ab059
kimyou1102 <yoosk5485@naver.com>
2023-05-29 00:53:32 +0900

    feat: router적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainPage, ShopDetailPage } from '@pages';
+import { MainPage, RestaurantDetailPage, ProductDetailPage, ProductListPage, RestaurantListPage } from '@pages';
 import { Header } from '@organisms';
 import App from './App';
 import GlobalStyle from './styles/globalStyle';
@@ -14,9 +14,12 @@ root.render(
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Header />}>
-          <Route path="/" element={<App />} />
-          <Route path="/shop-detail/:id" element={<ShopDetailPage />} />
-          <Route path="/home" element={<MainPage />} />
+          {/* <Route path="/" element={<App />} /> */}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/restaurant" element={<RestaurantListPage />} />
+          <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
+          <Route path="/product" element={<ProductListPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
